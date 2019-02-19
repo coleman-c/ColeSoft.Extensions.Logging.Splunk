dab8c1b6559443ddcac63d397fe9de8046a3b426
Chris Coleman <christopher.coleman@eon.com>
2019-02-19 17:03:05 +0000

    Formatting tidy up

---
+++ src/ColeSoft.Extensions.Logging.Splunk.Tests/TestLogger.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Microsoft.Extensions.Logging;
+
+namespace ColeSoft.Extensions.Logging.Splunk.Tests
+{
+    internal class TestLogger : ILogger
+    {
+        public IDisposable BeginScope<TState>(TState state)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool IsEnabled(LogLevel logLevel) => true;
+
+        public void Log<TState>(LogLevel logLevel, EventId eventId, TState state, Exception exception, Func<TState, Exception, string> formatter)
+        {
+            // No op
+        }
+    }
+}
---
+++ src/ColeSoft.Extensions.Logging.Splunk.Tests/TestLoggerProvider.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading.Tasks;
+using ColeSoft.Extensions.Logging.Splunk.Hec;
+using ColeSoft.Extensions.Logging.Splunk.Utils;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+
+namespace ColeSoft.Extensions.Logging.Splunk.Tests
+{
+    internal class TestLoggerProvider : SplunkProviderBase
+    {
+        public TestLoggerProvider(
+            IHttpClientProvider httpClientFactory,
+            IOptionsMonitor<SplunkLoggerOptions> options,
+            ISplunkLoggerProcessor loggerProcessor,
+            string endPointCustomization)
+            : base(httpClientFactory, options, loggerProcessor, endPointCustomization)
+        {
+        }
+
+        public HttpClient GetHttpClientForTestInspection() => HttpClient;
+
+        public override ILogger CreateLogger(string categoryName)
+        {
+            return new TestLogger();
+        }
+
+        protected override Task SendToSplunkAsync(IReadOnlyList<string> messages)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
